2a0fbba198b2af894e31a8229c483994836448c1
ntorbinskiy <n.torbinskiy@gmail.com>
2022-10-04 13:07:46 +0300

    Fixes of logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img
-					src={chrome.runtime.getURL(logo)}
+					src={logo}
 					className="App-logo"
 					alt="logo"
 				/>
